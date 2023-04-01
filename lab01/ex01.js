d0f26ca13acef299efe933efa4929e27b3639387
junghak <junghak3783@gmail.com>
2023-04-01 14:16:15 +0900

    commit

---
+++ lab01/ex01.js
@@ -0,0 +1,28 @@
+console.log("안녕하세요");
+
+var x = 1;
+x = "hello";
+
+console.log(x);
+
+var x = 100; // 동일한 이름의 변수 생성이 아닌 재할당이 이루어짐
+var x;
+console.log(x);
+
+let y = 100;
+
+const pi = 3.14;
+// pi = 3.1415;
+
+// 문자열 템플릿 (string template)
+let name = "john";
+let age = 20;
+let hello = `안녕하세요, ${name}. 제 나이는 ${age}입니다.`;
+
+console.log(hello);
+
+let a = 1; // 숫자 Type 1
+let b = "1"; // 문자 Type 1
+
+console.log(a == b);
+console.log(a === b);
---
+++ lab01/ex02.js
@@ -0,0 +1,5 @@
+// 조건문
+let age = 16;
+if (age < 20) {
+  console.log("미성년");
+}
